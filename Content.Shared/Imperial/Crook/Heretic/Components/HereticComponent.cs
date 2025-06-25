d9c36758fea42bc188724d5cb4b4c1b707e97a97
CrookLv <tpeckamail@gmail.com>
2025-06-25 17:06:27 +0800

    Update HereticComponent.cs

--- Content.Shared/Imperial/Crook/Heretic/Components/HereticComponent.cs
+++ Content.Shared/Imperial/Crook/Heretic/Components/HereticComponent.cs
@@ -6,9 +6,4 @@ namespace Content.Shared.Imperial.Heretic.Components;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class HereticComponent : Component
 {
-    /// <summary>
-    /// Максимальное количество рун, которые можно создать
-    /// </summary>
-    [DataField]
-    public int MaxRunes = 1;
 }
